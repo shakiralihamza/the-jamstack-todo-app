07271d691bfe0c1bfae505e787fcb1ece23cd9c3
Shakir Ali <shakiralihamza05@gmail.com>
2022-08-20 09:07:31 +0500

    Added netlify.toml

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,18 +1,18 @@
-import type { GatsbyConfig } from "gatsby"
+import type {GatsbyConfig} from "gatsby"
 
 const config: GatsbyConfig = {
-  siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
-  },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
-  graphqlTypegen: true,
-  plugins: [
-      'gatsby-plugin-top-layout',
-      'gatsby-plugin-mui-emotion',
-      'gatsby-plugin-react-helmet',
-  ],
+    siteMetadata: {
+        siteUrl: `https://www.yourdomain.tld`,
+    },
+    // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+    // If you use VSCode you can also use the GraphQL plugin
+    // Learn more at: https://gatsby.dev/graphql-typegen
+    graphqlTypegen: true,
+    plugins: [
+        'gatsby-plugin-top-layout',
+        'gatsby-plugin-mui-emotion',
+        'gatsby-plugin-react-helmet',
+    ],
 }
 
 export default config
