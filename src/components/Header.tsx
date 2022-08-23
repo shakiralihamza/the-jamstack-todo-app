f2bf488da181793f6b520d9e900e88803c2f5a05
Shakir Ali <shakiralihamza05@gmail.com>
2022-08-23 11:52:30 +0500

    title update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -50,7 +50,7 @@ export default function Header() {
                         <AppsIcon fontSize={'large'}/>
                     </IconButton>
                     <Typography variant="h5" fontWeight={100} sx={{flexGrow: 1}}>
-                        JAMStack ToDo App
+                        JAMStack Todo App
                     </Typography>
 
                     <Grid
