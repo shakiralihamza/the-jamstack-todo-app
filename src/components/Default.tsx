e206bb4be414c6717e15f7bad784a256efa01223
Shakir Ali <shakiralihamza05@gmail.com>
2022-08-23 11:48:22 +0500

    UI updates

--- src/components/Default.tsx
+++ src/components/Default.tsx
@@ -1,11 +1,57 @@
 import React from 'react';
 import Typography from "@mui/material/Typography";
+import {Grid, Stack} from "@mui/material";
+import {Button} from "gatsby-theme-material-ui";
+import netlifyIdentity from "netlify-identity-widget";
 
 function Default() {
     return (
-        <Typography variant={'body1'} sx={{mt:3, ml:3}}>
-            You need to login to access the app.
-        </Typography>
+        <Grid container justifyContent={'center'} alignContent={'center'} direction={'row'}
+              sx={{width: '100%', height: '400px'}} spacing={3}>
+            <Grid item xs={12}>
+                <Typography variant={'h2'} fontWeight={100} textAlign={'center'}>
+                    Just One Step Away!
+                </Typography>
+            </Grid>
+            <Grid item>
+                <Stack spacing={1}>
+                    <Button
+                        sx={{
+                            borderRadius: '20px',
+                            padding: '5px 20px',
+                            backgroundColor: 'primary.light',
+                            '&:hover': {
+                                backgroundColor: 'primary.main'
+                            },
+                            textTransform: 'none',
+                            fontWeight: '200',
+                        }}
+                        onClick={() => {
+                            netlifyIdentity.open('signup');
+                        }}
+                        disableRipple variant={'contained'}
+                    >
+                        Signup to Get Started
+                    </Button>
+                    <Button
+                        variant={'text'}
+                        sx={{
+                            color: 'primary.light',
+                            borderRadius: '20px',
+                            padding: '5px 20px',
+                            textTransform: 'none',
+                            fontWeight: '400',
+                        }}
+                        onClick={() => {
+                            netlifyIdentity.open('login');
+                        }}
+                        disableRipple
+                    >
+                        Already registered? Sign in
+                    </Button>
+                </Stack>
+            </Grid>
+        </Grid>
     );
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,43 @@
 import * as React from "react"
 import type {HeadFC} from "gatsby"
 import Layout from "../components/layout";
+import {Grid} from "@mui/material";
+import {Button} from "gatsby-theme-material-ui";
+import Typography from "@mui/material/Typography";
 
 const IndexPage = () => {
     return (
         <Layout>
-            <h1>Hello World</h1>
+            <Grid container justifyContent={'center'} alignContent={'center'} direction={'row'}
+                  sx={{width: '100%', height: '400px'}} spacing={3}>
+                <Grid item xs={12}>
+                    <Typography variant={'h2'} fontWeight={100} textAlign={'center'}>Accomplish more every
+                        day</Typography>
+                </Grid>
+                <Grid item xs={12}>
+                    <Typography variant={'body1'} fontWeight={100} textAlign={'center'}>
+                        Capture ideas, organize life, and do something creative everyday.
+                    </Typography>
+                </Grid>
+                <Grid item>
+                    <Button
+                        to={'/app'}
+                        sx={{
+                            borderRadius: '20px',
+                            padding: '5px 20px',
+                            backgroundColor: 'primary.light',
+                            '&:hover': {
+                                backgroundColor: 'primary.main'
+                            },
+                            textTransform: 'none',
+                            fontWeight: '400',
+                        }}
+                        disableRipple variant={'contained'}
+                    >
+                        Get Started - It's Free!
+                    </Button>
+                </Grid>
+            </Grid>
         </Layout>
     )
 }
