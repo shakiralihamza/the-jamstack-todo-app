5ee0b36832083b0163503ddee8d013c8ff295c72
Shakir Ali <shakiralihamza05@gmail.com>
2022-10-03 14:36:29 +0500

    index page responsive

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import type {HeadFC} from "gatsby"
 import Layout from "../components/layout";
-import {Grid} from "@mui/material";
+import {Grid, Stack} from "@mui/material";
 import {Button} from "gatsby-theme-material-ui";
 import Typography from "@mui/material/Typography";
 
@@ -9,8 +9,34 @@ const IndexPage = () => {
     return (
         <Layout>
             <Grid container justifyContent={'center'} alignContent={'center'} direction={'row'}
-                  sx={{width: '100%', height: '400px'}} spacing={3}>
-                <Grid item xs={12}>
+                  sx={{width: '100%', height: '400px', px: {xs: 3, md: 0}}}>
+                <Stack spacing={3}>
+                    <Typography variant={'h2'} fontWeight={100} textAlign={'center'} sx={{
+                        fontSize: {xs: '2.5rem', sm: '3.0', md: '3.8rem'},
+                    }}>
+                        Accomplish more every day
+                    </Typography>
+                    <Typography variant={'body1'} fontWeight={100} textAlign={'center'}>
+                        Capture ideas, organize life, and do something creative everyday.
+                    </Typography>
+                    <Button
+                        to={'/app'}
+                        sx={{
+                            borderRadius: '20px',
+                            padding: '5px 20px',
+                            backgroundColor: 'primary.light',
+                            '&:hover': {
+                                backgroundColor: 'primary.main'
+                            },
+                            textTransform: 'none',
+                            fontWeight: '400',
+                        }}
+                        disableRipple variant={'contained'}
+                    >
+                        Get Started - It's Free!
+                    </Button>
+                </Stack>
+                {/*   <Grid item xs={12}>
                     <Typography variant={'h2'} fontWeight={100} textAlign={'center'}>Accomplish more every
                         day</Typography>
                 </Grid>
@@ -36,7 +62,7 @@ const IndexPage = () => {
                     >
                         Get Started - It's Free!
                     </Button>
-                </Grid>
+                </Grid>*/}
             </Grid>
         </Layout>
     )
